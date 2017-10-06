f6b3174905b8cb06777ab80dd4e87234cf6be241
ChandruCRM <mailchandrucr@gmail.com>
2017-10-06 11:28:41 +0200

    First Commit - SVG Polyline Angular

--- README.md
+++ README.md
@@ -2,27 +2,21 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.4.2.
 
+It contains an angular based application which can be used to draw svg polyline dynamically by providing inputs from screen.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+## Instructions
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+svgHeight: Height of Polyline graph which will drawn on the screen.
+svgWidth: Width of Polyline graph which will drawn on the screen.
+transMatrix: Matrix for zooming and panning of the  graph.
+tansform: transMatrix in string format which will be used in html.
 
-## Build
+Initialize above values as per your needs and run the application.
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-Before running the tests make sure you are serving the app via `ng serve`.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+## Author
+Chandru C R M
+mailchandrucr@gmail.com - https://github.com/ChandruCR/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,59 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  svgHeight: any; //height of svg
+  svgWidth: any;  //width of svg
+  transMatrix: number[] // matrix value for transformation
+  transform: string; // matrix value in string format
+  points: any[]; // all points are stored in this
+  polyline: string; // all points for polyline in string format are stored in this
+  cx: number; // x co-ordinate value entered on screen
+  cy: number; // y co-ordinate value entered on screen
+  isPointsAdded: boolean; // to display svg only after points are added by user
+  isValidValues: boolean; // to change color of info when user enters invalid values
+
+  constructor() { // initialize values here as per your needs
+    this.svgHeight = 760;
+    this.svgWidth = 1020;
+    this.transMatrix  = [1, 0, 0, 1, 0, 0];
+    this.transform = "matrix(1 0 0 1 0 0)";
+    this.points = [ ];
+    this.polyline = "";
+    this.isPointsAdded = false;
+    this.isValidValues = true;
+  }
+
+  addPoint() { // called when Add button is clicked. Adds cx and cy to points and polyline and sets them to null
+    if (this.cx == null || this.cy == null || this.cx == undefined || this.cy == undefined || this.cx < 0 || this.cy < 0 || this.cx > this.svgWidth || this.cy > this.svgHeight)
+      this.isValidValues = false;
+    else {
+      this.points.push({ "cx": this.cx, "cy": this.cy });
+      this.polyline = this.polyline + " " + this.cx + "," + this.cy;
+      this.isPointsAdded = true;
+      this.isValidValues = true;
+      this.cx = null;
+      this.cy = null;
+    }
+  }
+
+  pan(dx, dy) { // used for panning left, right, top and bottom. Changes matrix 5th and 6th position by a fixed value based on button clicked
+    this.transMatrix[4] += dx;
+    this.transMatrix[5] += dy;
+
+    this.transform = "matrix(" + this.transMatrix.join(' ') + ")";
+  }
+
+
+  zoom(scale) { // used for zoom in and zoom out. Multiplies matrix values by a fixed value and pans accordingly.
+    for (var i = 0; i < this.transMatrix.length; i++) {
+      this.transMatrix[i] *= scale;
+    }
+    this.transMatrix[4] += (1 - scale) * this.svgWidth / 2;
+    this.transMatrix[5] += (1 - scale) * this.svgHeight / 2;
+
+    this.transform = "matrix(" + this.transMatrix.join(' ') + ")";
+  }
 }
